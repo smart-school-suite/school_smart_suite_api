5c5fecf1955b5881799a8fbb79d9089cd34fc114
chongongprecious <chongongprecious@gmail.com>
2024-09-02 03:29:55 +0100

    password reset functionality

---
+++ app/Http/Controllers/Auth/Student/resetpasswordController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Student;
+
+use App\Http\Controllers\Controller;
+use App\Models\Student;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+class resetpasswordController extends Controller
+{
+    //
+    public function reset_password(Request $request){
+        $request->validate([
+            'password' => 'required|min:6|confirmed',
+        ]);
+    
+        // Retrieve email from the session
+        $email = session('password_reset_email');
+         
+        $new_password = Hash::make($request->password);
+
+        Student::where('email', $email)->update(['password' => $new_password]);
+        
+        session()->forget('password_reset_email');
+
+        return response()->json(['message' => 'Password has been successfully reset'], 200);
+        
+    }
+}
