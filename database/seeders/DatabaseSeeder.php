bc81f974f8b6217cdbdf43c01e90d89f5667a35f
keron484 <chongongprecious@gmail.com>
2026-01-27 10:18:45 +0000

    fix: fixed bug on the database seeder for activation code type seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\ActivationCodeType;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -33,7 +31,7 @@ class DatabaseSeeder extends Seeder
         $this->call(SettingDefinationSeeder::class);
         $this->call(StudentParentRelationshipSeeder::class);
         $this->call(LevelSeeder::class);
-        $this->call(ActivationCodeType::class);
+        $this->call(ActivationCodeTypeSeeder::class);
         $this->call(FeaturePlanSeeder::class);
         $this->call(StudentCredentials::class);
     }
